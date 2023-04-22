4d1998faad881c9ff29c655a41e739ac56e5348e
YerbolZhulniyazov <zhulniyazov@gmail.com>
2023-04-22 19:00:27 +0600

    #3 Добавил views аккаунтов
    Написал вьюхи для регистрации, входа, редактирования и метод для выхода

---
+++ source/accounts/views.py
@@ -0,0 +1,87 @@
+from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, ListView
+
+from gallery.models import Image
+from .forms import LoginForm, CustomUserCreationForm, UserChangeForm
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form(form_data)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        next = form.cleaned_data.get('next')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'profile.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        images = Image.objects.filter(users=self.request.user)
+        context['images'] = images
+        return context
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.object.pk})
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        user = form.save()
+        update_session_auth_hash(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
